56bfb6fa36a52b9bd8a36410eb3c08f85cb23ccb
tosiek12 <antek.trad@gmail.com>
2016-03-08 09:54:32 +0100

    exclude js file from table.htm -

---
+++ TechnologieInternetowe/zajecia/js/zajecia.js
@@ -0,0 +1,12 @@
+﻿function koloruj() {
+    var elems = document.getElementsByClassName("pasmo");
+
+    for (var i = 0; i < elems.length; i++) {
+        elems[i].setAttribute('bgcolor', '#00FF00');
+    }
+};
+function dodajObraz() {
+    var elem = document.getElementById("wstaw");
+    elem.innerHTML = "<h1>tst</h1>";
+    elem.innerHTML = "<img src='grafika.jpg' border='0' width='200' height='130'/>";
+};
