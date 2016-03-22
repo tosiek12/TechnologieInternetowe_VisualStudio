3725047a718f216fa733a271fd7874b47aa44ac9
tosiek12 <antek.trad@gmail.com>
2016-03-22 10:18:20 +0100

    mysql query

--- TechnologieInternetowe/zajecia/template.php
+++ TechnologieInternetowe/zajecia/template.php
@@ -14,16 +14,32 @@
 	echo $content;
 	?>
 
+	<?php
+		$servername = "mysql.agh.edu.pl";
+		$username = "atrad";
+		$password = "GSnx6QgH";
+		$dbname = "atrad";
+
+		// Create connection
+		$conn = new mysqli($servername, $username, $password, $dbname);
+		// Check connection
+		if ($conn->connect_error) {
+			die("Connection failed: " . $conn->connect_error);
+		}
+
+		$sql = "SELECT id, firstname, lastname FROM MyGuests";
+		$result = $conn->query($sql);
+
+		if ($result->num_rows > 0) {
+			// output data of each row
+			while($row = $result->fetch_assoc()) {
+				echo "id: " . $row["id"]. " - Name: " . $row["firstname"]. " " . $row["lastname"]. "<br>";
+			}
+		} else {
+			echo "0 results";
+		}
+		$conn->close();
+	?> 
+		
     </body>
 </html>
-<!--
-	Todo:
-    http://www.w3schools.com/css/css_navbar.asp
-    
-	Poszukac:
-	1) roznice html5 vs xtml
-	2) roznice css vs css3
-	nastepne:
-	1) css + js
-
--->
