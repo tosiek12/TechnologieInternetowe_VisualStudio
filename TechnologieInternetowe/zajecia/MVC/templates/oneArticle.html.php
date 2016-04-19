ace4b8944b4988b6ef4b3cc87ad9f70ef1c92c44
Antonio-laptop <Antonio-laptop@ANTONIO-LAPTOP>
2016-04-19 03:22:10 +0200

    ocenianie wpisow

--- TechnologieInternetowe/zajecia/MVC/templates/oneArticle.html.php
+++ TechnologieInternetowe/zajecia/MVC/templates/oneArticle.html.php
@@ -13,6 +13,39 @@ includeHeader();
 	<p><?= $articles['content']; ?></p>
 
 		<? } ?>
+    
+    <table>
+      <tr>
+        <td>Ocen artykul: </td>
+        <td>
+          <?php require_once 'Functions.php';
+          if(!isLogged()){
+          echo('Musisz sie zalogowac');
+          }else {
+           echo('<button class="button button1" onClick="ocen('.$articles['id'].',3)">Super!</button>');
+           echo('<button class="button button2" onClick="ocen('.$articles['id'].',2)">Ok.</button>');
+           echo('<button class="button button3" onClick="ocen('.$articles['id'].',1)">Slabo...</button>');
+          }?>
+          
+          <script type="text/javascript">
+            function ocen(id,ocena) {
+              console.log('test');
+              $.ajax({
+                url: "welcome_post.php",
+                type: "POST",
+                async: true,
+                data: { sql:"(" + id + "," + ocena +")"},
+                dataType: "html",
+
+                success: function(data) {
+                    console.log('succes');
+                }
+              });
+            }
+          </script>
+        </td>
+      </tr>
+    </table>
 </div>
 
 <?php includeFooter(); ?>
--- TechnologieInternetowe/zajecia/MVC/welcome_post.php
+++ TechnologieInternetowe/zajecia/MVC/welcome_post.php
@@ -7,6 +7,27 @@ if(isset($_POST['submitLogin']))
 } else if(isset($_POST['submitLogin']))
 {
    saveAndRedirect();
+}else if(isset($_POST['sql']))
+{
+  try {
+      require 'config/sql.php';
+      
+      $conn=new mysqli($host, $user, $pass, $dbase);
+      // Check connection
+		  if ($conn->connect_error) {
+        throw new Exception('Connection failed: '.$conn->connect_error);
+		  }
+  }
+  catch(DBException $e) {
+      echo 'The connect can not create: ' . $e->getMessage();
+  }
+  
+  $sql = "INSERT INTO actionLog (userName, action) VALUES ".$_POST['sql'];
+  
+  echo ($sql);
+  $ins=$conn->prepare($sql);
+  $ins->execute();
+  $ins->close();
 }
 
 
